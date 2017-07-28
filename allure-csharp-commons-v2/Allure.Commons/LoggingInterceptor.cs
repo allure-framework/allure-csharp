486b2506c461efe2f1a6463582edf417c59bc0e7
Bakanych <manych@gmail.com>
2017-07-28 12:37:50 +0800

    Logging

---
+++ allure-csharp-commons-v2/Allure.Commons/LoggingInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using Castle.DynamicProxy;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Allure.Commons
+{
+    class LoggingInterceptor : IInterceptor
+    {
+        private Logger logger = LogManager.GetCurrentClassLogger();
+
+        internal LoggingInterceptor()
+        {
+            logger.Info("New Allure Lifecycle");
+        }
+        public void Intercept(IInvocation invocation)
+        {
+            var message = new StringBuilder();
+            message
+                .Append($"[{invocation.InvocationTarget.GetHashCode()}] ")
+                .Append(invocation.Method.Name)
+                .Append(" (")
+                .Append(string.Join(", ", invocation.Arguments))
+                .Append(")");
+
+            logger.Info(message.ToString());
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                throw;
+            }
+        }
+    }
+}
