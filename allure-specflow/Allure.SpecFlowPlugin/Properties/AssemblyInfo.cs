51de7bd451ed406b4a55f1edb44865e3efe43541
Bakanych <manych@gmail.com>
2017-08-04 14:24:04 +0800

    nuget

--- allure-specflow/Allure.SpecFlowPlugin/Properties/AssemblyInfo.cs
+++ allure-specflow/Allure.SpecFlowPlugin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Allure.SpecFlowPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SpecFlow.Allure")]
+[assembly: AssemblyDescription("SpecFlow plugin to generate Yandex Allure report results during SpecFlow test run")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Allure.SpecFlowPlugin")]
+[assembly: AssemblyCompany("Alexander Bakanov")]
+[assembly: AssemblyProduct("SpecFlow.Allure")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
