dc3794f3155f70549198b7a970ad3890ddc575b8
Nick Chursin <nicktestqa@yahoo.com>
2017-11-03 19:30:28 +0200

    SpecFlow 2.2 support

--- Allure.SpecFlowPlugin/AllureBindings.cs
+++ Allure.SpecFlowPlugin/AllureBindings.cs
@@ -1,9 +1,4 @@
 ﻿using Allure.Commons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
 namespace Allure.SpecFlowPlugin
