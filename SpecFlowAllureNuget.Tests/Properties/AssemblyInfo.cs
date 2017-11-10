1cb24ab817df28ac026711441553b08c51321986
Bakanych <manych@gmail.com>
2017-11-10 19:29:22 +0800

    tests

--- SpecFlowAllureNuget.Tests/Properties/AssemblyInfo.cs
+++ SpecFlowAllureNuget.Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Allure.SpecFlowPlugin.NugetTest")]
+[assembly: AssemblyTitle("SpecFlowAllureNuget.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Allure.SpecFlowPlugin.NugetTest")]
+[assembly: AssemblyProduct("SpecFlowAllureNuget.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("c5bdb491-f2ef-4d3e-9aba-595c74673df6")]
+[assembly: Guid("85755452-928c-43a3-a020-00117b7a00e2")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ SpecFlowAllureNuget.Tests/SpecFlowFeature1Steps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowAllureNuget.Tests
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+
+        }
+    }
+}
