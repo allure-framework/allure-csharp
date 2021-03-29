097061df04e87208e3d59f35ef0296abea1ae800
Bakanych <manych@gmail.com>
2021-03-29 13:20:55 +0800

    fix running integration tests

--- Allure.Features/runtests.cmd
+++
@@ -1,5 +0,0 @@
-@pushd %~dp0
-@set profile=%1
-@if "%profile%" == "" set profile=Default
-SpecFlowPlusRunner\SpecRun.exe run %profile%.srprofile "/outputFolder:%~dp0/TestResults/" /log:specrun.log %2 %3 %4 %5
-@popd
