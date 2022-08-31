70383aed99a9f195d97b55ec50bc8313e783afbe
Aleksandr Kulakov <a.kulakov@dodopizza.com>
2022-08-31 13:21:18 +0500

    add allureId attribute

---
+++ Allure.NUnit/Attributes/AllureIdAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Allure.Net.Commons;
+
+namespace NUnit.Allure.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AllureIdAttribute : AllureTestCaseAttribute
+    {
+        public AllureIdAttribute(int id)
+        {
+            Id = id;
+        }
+
+        private int Id { get; }
+
+        public override void UpdateTestResult(TestResult testResult)
+        {
+            testResult.labels.Add(new Label {name = "AS_ID", value = Id.ToString()});
+        }
+    }
+}
