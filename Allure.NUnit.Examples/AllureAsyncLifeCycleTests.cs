c0fd75e0ad9a465a5c524cdfcb1cff400ca0b17a
Overlord <overlord1983@mail.ru>
2022-12-27 00:59:13 +0300

    Add support for async NUnit tests #303

---
+++ Allure.NUnit.Examples/AllureAsyncLifeCycleTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Allure.Net.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+
+namespace Allure.NUnit.Examples
+{
+    [AllureNUnit]
+    public class AllureAsyncLifeCycleTests
+    {
+        [Test]
+        public async Task AsyncTest_With_AllureStepAttribute()
+        {
+            Console.WriteLine("> Сalling async method with [AllureStep] ...");
+            await StepWithAttribute();
+            Console.WriteLine("  Done!");
+        }
+
+        [AllureStep("Calling StepWithAttribute")]
+        private async Task StepWithAttribute()
+        {
+            // switching thread on async
+            Console.WriteLine($"  > Delay...");
+            await Task.Delay(1000);
+            Console.WriteLine($"    Done!");
+
+            // use internally allure steps storage on different thread
+            Console.WriteLine($"  > AddAttachment...");
+            AllureLifecycle.Instance.AddAttachment("attachment-name", "text/plain", Encoding.UTF8.GetBytes("attachment-value"));
+            Console.WriteLine($"    Done!");
+        }
+
+        [Test]
+        public async Task AsyncTest_With_WrapInStep()
+        {
+            Console.WriteLine("> StepLevel1...");
+            await AllureLifecycle.Instance.WrapInStepAsync(
+                async () => await StepLevel1()
+            );
+            Console.WriteLine("  Done!");
+        }
+
+        private async Task StepLevel1()
+        {
+            Console.WriteLine("  > StepLevel2...");
+            await AllureLifecycle.Instance.WrapInStepAsync(
+                async () => await StepLevel2()
+            );
+            Console.WriteLine("    Done!");
+        }
+
+        private async Task StepLevel2()
+        {
+            // switching thread on async
+            Console.WriteLine($"    > Sleep...");
+            await Task.Delay(1000);
+            Console.WriteLine($"      Done!");
+
+            // use internally allure steps storage on different thread
+            Console.WriteLine($"    > AddAttachment...");
+            AllureLifecycle.Instance.AddAttachment("attachment-name", "text/plain", Encoding.UTF8.GetBytes("attachment-value"));
+            Console.WriteLine($"      Done!");
+        }
+    }
+}
