5a161a03cb1ebdaf9f78a60cb0963555a8099c20
Constantine <overlord1983@mail.ru>
2023-02-13 11:04:01 +0300

    [NUnit] Fix async behaviour in [AllureStep] aspect + Allow '{obj.prop}' placeholders in [AllureStep] step name (#329)
    Co-authored-by: Кабанов Константин Юрьевич <kykabano@mts.ru>

--- Allure.NUnit/Core/Steps/AllureStepAspect.cs
+++ Allure.NUnit/Core/Steps/AllureStepAspect.cs
@@ -19,107 +19,68 @@ namespace NUnit.Allure.Core.Steps
 
         private static readonly MethodInfo SyncHandler =
             typeof(AllureStepAspect).GetMethod(nameof(WrapSync), BindingFlags.NonPublic | BindingFlags.Static);
-        
-        private static readonly Type _voidTaskResult = Type.GetType("System.Threading.Tasks.VoidTaskResult");
+
+        private static readonly MethodInfo SyncVoidHandler =
+            typeof(AllureStepAspect).GetMethod(nameof(WrapSyncVoid), BindingFlags.NonPublic | BindingFlags.Static);
+
+        private static readonly Type _typeVoidTaskResult = Type.GetType("System.Threading.Tasks.VoidTaskResult");
+        private static readonly Type _typeVoid = typeof(void);
+        private static readonly Type _typeTask = typeof(Task);
 
         [Advice(Kind.Around, Targets = Target.Method)]
-        public object Around([Argument(Source.Name)] string name,
+        public object Around(
+            [Argument(Source.Name)] string name,
             [Argument(Source.Arguments)] object[] args,
             [Argument(Source.Target)] Func<object[], object> target,
             [Argument(Source.Metadata)] MethodBase metadata,
-            [Argument(Source.ReturnType)] Type returnType)
+            [Argument(Source.ReturnType)] Type returnType
+        )
         {
-            object executionResult;
-            var stepName = metadata.GetCustomAttribute<AllureStepBaseAttribute>().Name ?? name;
+            var stepNamePattern = metadata.GetCustomAttribute<AllureStepBaseAttribute>().Name ?? name;
+            var stepName = AllureStepParameterHelper.GetStepName(stepNamePattern, metadata, args);
+            var stepParameters = GetStepParameters(metadata, args);
 
-            for (var i = 0; i < args.Length; i++)
+            if (_typeTask.IsAssignableFrom(returnType))
             {
-                stepName = stepName?.Replace("{" + i + "}", args[i]?.ToString() ?? "null");
-                if (stepName.Contains("{" + i + "}"))
-                {
-                    // TODO: provide error description link
-                    Console.Error.WriteLine("Indexed step arguments is obsolete. Use named arguments instead. ({0} -> {argumentName}) See: LINK_TO_ERROR");
-                }
+                var syncResultType = returnType.IsConstructedGenericType
+                    ? returnType.GenericTypeArguments[0]
+                    : _typeVoidTaskResult;
+                return AsyncHandler.MakeGenericMethod(syncResultType)
+                    .Invoke(this, new object[] { target, args, metadata, stepName, stepParameters });
             }
-            
-            stepName = metadata.GetParameters().Aggregate(stepName,
-                (current, parameterInfo) => current?.Replace("{" + parameterInfo.Name + "}",
-                    args[parameterInfo.Position]?.ToString() ?? "null"));
-
-            var stepResult = string.IsNullOrEmpty(stepName)
-                ? new StepResult {name = name, parameters = AllureStepParameterHelper.CreateParameters(args)}
-                : new StepResult {name = stepName, parameters = AllureStepParameterHelper.CreateParameters(args)};
-
-            var stepParameters = metadata.GetParameters()
-                .Select(x => (
-                    name: x.GetCustomAttribute<NameAttribute>()?.Name ?? x.Name,
-                    skip: x.GetCustomAttribute<SkipAttribute>() != null))
-                .Zip(args, (parameter, value) => parameter.skip
-                    ? null
-                    : new Parameter
-                    {
-                        name = parameter.name,
-                        value = value?.ToString()
-                    })
-                .Where(x => x != null)
-                .ToList();
-
-            try
+            else if (_typeVoid.IsAssignableFrom(returnType))
             {
-                StartFixture(metadata, stepName);
-                StartStep(metadata, stepName, stepParameters);
-
-                executionResult = GetStepExecutionResult(returnType, target, args);
-                if (executionResult != null && typeof(Task).IsAssignableFrom(executionResult.GetType()))
-                {
-                    ((Task)executionResult).ContinueWith((task) =>
-                    {
-                        if (task.IsFaulted)
-                        {
-                            var e = task.Exception;
-                            ThrowStep(metadata, e?.InnerException);
-                            ThrowFixture(metadata, e?.InnerException);
-                        }
-                        else
-                        {
-                            PassStep(metadata);
-                            PassFixture(metadata);
-                        }
-                    });
-                }
-                else
-                {
-                    PassStep(metadata);
-                    PassFixture(metadata);
-                }
+                return SyncVoidHandler
+                    .Invoke(this, new object[] { target, args, metadata, stepName, stepParameters });
             }
-            catch (Exception e)
+            else
             {
-                ThrowStep(metadata, e);
-                ThrowFixture(metadata, e);
-                throw;
+                return SyncHandler.MakeGenericMethod(returnType)
+                    .Invoke(this, new object[] { target, args, metadata, stepName, stepParameters });
             }
-
-            return executionResult;
         }
-        
-        private static void StartStep(MethodBase metadata, string stepName, List<Parameter> stepParameters)
+
+        // ------------------------------
+
+        private static string StartStep(MethodBase metadata, string stepName, List<Parameter> stepParameters)
         {
             if (metadata.GetCustomAttribute<AllureStepAttribute>() != null)
             {
-                StepsHelper.StartStep(stepName, step => step.parameters = stepParameters);
+                return StepsHelper.StartStep(stepName, step => step.parameters = stepParameters);
             }
+
+            return null;
         }
-        
-        private static void PassStep(MethodBase metadata)
+
+        private static void PassStep(string uuid, MethodBase metadata)
         {
             if (metadata.GetCustomAttribute<AllureStepAttribute>() != null)
             {
-                StepsHelper.PassStep();
+                StepsHelper.PassStep(uuid);
             }
         }
-        
-        private static void ThrowStep(MethodBase metadata, Exception e)
+
+        private static void ThrowStep(string uuid, MethodBase metadata, Exception e)
         {
             if (metadata.GetCustomAttribute<AllureStepAttribute>() != null)
             {
@@ -128,18 +89,18 @@ namespace NUnit.Allure.Core.Steps
                     message = e.Message,
                     trace = e.StackTrace
                 };
-                
+
                 if (e is NUnitException || e is AssertionException)
                 {
-                    StepsHelper.FailStep(result => result.statusDetails = exceptionStatusDetails);
+                    StepsHelper.FailStep(uuid, result => result.statusDetails = exceptionStatusDetails);
                 }
                 else
                 {
-                    StepsHelper.BrokeStep(result => result.statusDetails = exceptionStatusDetails);
+                    StepsHelper.BrokeStep(uuid, result => result.statusDetails = exceptionStatusDetails);
                 }
             }
         }
-        
+
         private static void StartFixture(MethodBase metadata, string stepName)
         {
             if (metadata.GetCustomAttribute<AllureBeforeAttribute>() != null)
@@ -152,7 +113,7 @@ namespace NUnit.Allure.Core.Steps
                 StepsHelper.StartAfterFixture(stepName);
             }
         }
-        
+
         private static void PassFixture(MethodBase metadata)
         {
             if (metadata.GetCustomAttribute<AllureBeforeAttribute>() != null ||
@@ -161,7 +122,7 @@ namespace NUnit.Allure.Core.Steps
                 StepsHelper.StopFixtureSuppressTestCase(result => result.status = Status.passed);
             }
         }
-        
+
         private static void ThrowFixture(MethodBase metadata, Exception e)
         {
             if (metadata.GetCustomAttribute<AllureBeforeAttribute>() != null ||
@@ -192,34 +153,120 @@ namespace NUnit.Allure.Core.Steps
             }
         }
 
-        private object GetStepExecutionResult(Type returnType, Func<object[], object> target, object[] args)
+        // ------------------------------
+
+        private List<Parameter> GetStepParameters(MethodBase metadata, object[] args)
         {
-            if (typeof(Task).IsAssignableFrom(returnType))
-            {
-                var syncResultType = returnType.IsConstructedGenericType
-                    ? returnType.GenericTypeArguments[0]
-                    : _voidTaskResult;
-                return AsyncHandler.MakeGenericMethod(syncResultType)
-                    .Invoke(this, new object[] { target, args });
-            }
-
-            if (typeof(void).IsAssignableFrom(returnType))
-            {
-                return target(args);
-            }
-
-            return SyncHandler.MakeGenericMethod(returnType)
-                .Invoke(this, new object[] { target, args });
+            return metadata.GetParameters()
+                .Select(x => (
+                    name: x.GetCustomAttribute<NameAttribute>()?.Name ?? x.Name,
+                    skip: x.GetCustomAttribute<SkipAttribute>() != null))
+                .Zip(args,
+                    (parameter, value) => parameter.skip
+                        ? null
+                        : new Parameter
+                        {
+                            name = parameter.name,
+                            value = value?.ToString()
+                        })
+                .Where(x => x != null)
+                .ToList();
         }
 
-        private static T WrapSync<T>(Func<object[], object> target, object[] args)
+        // ------------------------------
+
+        private static string BeforeTargetInvoke(MethodBase metadata, string stepName, List<Parameter> stepParameters)
         {
-            return (T)target(args);
+            StartFixture(metadata, stepName);
+            var stepUuid = StartStep(metadata, stepName, stepParameters);
+            return stepUuid;
         }
 
-        private static async Task<T> WrapAsync<T>(Func<object[], object> target, object[] args)
+        private static void AfterTargetInvoke(string stepUuid, MethodBase metadata)
         {
-            return await (Task<T>)target(args);
+            PassStep(stepUuid, metadata);
+            PassFixture(metadata);
+        }
+
+        private static void OnTargetInvokeException(string stepUuid, MethodBase metadata, Exception e)
+        {
+            ThrowStep(stepUuid, metadata, e);
+            ThrowFixture(metadata, e);
+        }
+
+        // ------------------------------
+
+        private static T WrapSync<T>(
+            Func<object[], object> target,
+            object[] args,
+            MethodBase metadata,
+            string stepName,
+            List<Parameter> stepParameters
+        )
+        {
+            string stepUuid = null;
+
+            try
+            {
+                stepUuid = BeforeTargetInvoke(metadata, stepName, stepParameters);
+                var result = (T)target(args);
+                AfterTargetInvoke(stepUuid, metadata);
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                OnTargetInvokeException(stepUuid, metadata, e);
+                throw;
+            }
+        }
+
+        private static void WrapSyncVoid(
+            Func<object[], object> target,
+            object[] args,
+            MethodBase metadata,
+            string stepName,
+            List<Parameter> stepParameters
+        )
+        {
+            string stepUuid = null;
+
+            try
+            {
+                stepUuid = BeforeTargetInvoke(metadata, stepName, stepParameters);
+                target(args);
+                AfterTargetInvoke(stepUuid, metadata);
+            }
+            catch (Exception e)
+            {
+                OnTargetInvokeException(stepUuid, metadata, e);
+                throw;
+            }
+        }
+
+        private static async Task<T> WrapAsync<T>(
+            Func<object[], object> target,
+            object[] args,
+            MethodBase metadata,
+            string stepName,
+            List<Parameter> stepParameters
+        )
+        {
+            string stepUuid = null;
+
+            try
+            {
+                stepUuid = BeforeTargetInvoke(metadata, stepName, stepParameters);
+                var result = await ((Task<T>)target(args)).ConfigureAwait(false);
+                AfterTargetInvoke(stepUuid, metadata);
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                OnTargetInvokeException(stepUuid, metadata, e);
+                throw;
+            }
         }
     }
 }
