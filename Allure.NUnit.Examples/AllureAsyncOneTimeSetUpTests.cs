ea894d96eb9e83e0b364061dca8459fc8983f28b
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2023-09-09 01:26:30 +0700

    Fix indentation for AllureAsyncOneTimeSetUoTests.cs

--- Allure.NUnit.Examples/AllureAsyncOneTimeSetUpTests.cs
+++ Allure.NUnit.Examples/AllureAsyncOneTimeSetUpTests.cs
@@ -6,39 +6,39 @@ using NUnit.Framework;
 namespace Allure.NUnit.Examples
 {
     [AllureSuite("Tests - Async OneTime SetUp")]
-	[Parallelizable(ParallelScope.All)]
-	public class AllureAsyncOneTimeSetUpTests: BaseTest
-	{
-		[OneTimeSetUp]
-		[AllureBefore]
-		public async Task OneTimeSetUp()
-		{
+    [Parallelizable(ParallelScope.All)]
+    public class AllureAsyncOneTimeSetUpTests: BaseTest
+    {
+        [OneTimeSetUp]
+        [AllureBefore]
+        public async Task OneTimeSetUp()
+        {
             await AsyncStepsExamples.PrepareDough();
-			await AsyncStepsExamples.CookPizza();
-			await AsyncStepsExamples.CookPizza();
-			await AsyncStepsExamples.CookPizza();
-		}
+            await AsyncStepsExamples.CookPizza();
+            await AsyncStepsExamples.CookPizza();
+            await AsyncStepsExamples.CookPizza();
+        }
 
         [SetUp]
         [AllureBefore]
-		public async Task SetUp()
-		{
+        public async Task SetUp()
+        {
             await AsyncStepsExamples.PrepareDough();
-		}
+        }
 
         [Test]
-		[AllureName("Test1")]
-		public async Task Test1()
-		{
+        [AllureName("Test1")]
+        public async Task Test1()
+        {
             await AsyncStepsExamples.DeliverPizza();
-			await AsyncStepsExamples.Pay();
-		}
-		
-		[Test]
-		[AllureName("Test2")]
-		public async Task Test2()
-		{
+            await AsyncStepsExamples.Pay();
+        }
+        
+        [Test]
+        [AllureName("Test2")]
+        public async Task Test2()
+        {
             await AsyncStepsExamples.DeliverPizza();
-		}
-	}
+        }
+    }
 }
