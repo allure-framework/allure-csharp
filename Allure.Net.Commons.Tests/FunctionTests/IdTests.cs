4d1a2851b569fdb38d93726fc3073d50991ed41d
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2023-10-05 00:30:58 +0700

    Implement UUID and method-based fullName algorithms

---
+++ Allure.Net.Commons.Tests/FunctionTests/IdTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Allure.Net.Commons.Functions;
+using NUnit.Framework;
+
+namespace Allure.Net.Commons.Tests.FunctionTests;
+
+class IdTests
+{
+    [Test]
+    public void TestUUIDGeneration()
+    {
+        string generatedUuid = IdFunctions.CreateUUID();
+
+        Assert.That(
+            () => Guid.ParseExact(generatedUuid, "d"),
+            Throws.Nothing
+        );
+    }
+
+    class MyClass
+    {
+        internal void ParameterlessMethod() { }
+        internal void MethodWithParameterOfBuiltInType(int _) { }
+        internal void MethodWithParameterOfUserType(MyClass _) { }
+        internal void MethodWithTwoParameters(int _, MyClass __) { }
+        internal void MethodWithRefParameter(ref int _) { }
+        internal void MethodWithGenericParameter<T>() { }
+        internal void MethodWithArgumentOfGenericType<T>(T _) { }
+        internal void MethodWithArgumentOfTypeParametrizedByGenericType<T>(Dictionary<int, T> _) { }
+        internal void MethodWithArgumentOfGenericUserType<T>(MyClass<T> _) { }
+    }
+
+    class MyClass<T>
+    {
+        internal void GenericMethodOfGenericClass<V>(List<T> _, List<V> __) { }
+    }
+
+    [TestCase(
+        nameof(MyClass.ParameterlessMethod),
+        "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass.ParameterlessMethod()",
+        TestName = "ParameterlessMethod"
+    )]
+    [TestCase(
+        nameof(MyClass.MethodWithParameterOfBuiltInType),
+        "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass.MethodWithParameterOfBuiltInType(System.Int32)",
+        TestName = "MethodWithParameterOfBuiltInType"
+    )]
+    [TestCase(
+        nameof(MyClass.MethodWithParameterOfUserType),
+        "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass.MethodWithParameterOfUserType(Allure.Net." +
+            "Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass)",
+        TestName = "MethodWithParameterOfUserType"
+    )]
+    [TestCase(
+        nameof(MyClass.MethodWithTwoParameters),
+        "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass.MethodWithTwoParameters(System.Int32,Allure." +
+            "Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass)",
+        TestName = "MethodWithTwoParameters"
+    )]
+    [TestCase(
+        nameof(MyClass.MethodWithRefParameter),
+        "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass.MethodWithRefParameter(System.Int32&)",
+        TestName = "MethodWithRefParameter"
+    )]
+    [TestCase(
+        nameof(MyClass.MethodWithGenericParameter),
+        "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass.MethodWithGenericParameter[T]()",
+        TestName = "MethodWithGenericParameter"
+    )]
+    [TestCase(
+        nameof(MyClass.MethodWithArgumentOfGenericType),
+        "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass.MethodWithArgumentOfGenericType[T](T)",
+        TestName = "MethodWithArgumentOfGenericType"
+    )]
+    [TestCase(
+        nameof(MyClass.MethodWithArgumentOfTypeParametrizedByGenericType),
+        "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass.MethodWithArgumentOfTypeParametrizedByGenericType" +
+            "[T](System.Collections.Generic.Dictionary`2[System.Int32,T])",
+        TestName = "MethodWithArgumentOfTypeParametrizedByGenericType"
+    )]
+    [TestCase(
+        nameof(MyClass.MethodWithArgumentOfGenericUserType),
+        "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+            "IdTests+MyClass.MethodWithArgumentOfGenericUserType" +
+            "[T](Allure.Net.Commons.Tests:Allure.Net.Commons.Tests." +
+            "FunctionTests.IdTests+MyClass`1[T])",
+        TestName = "MethodWithArgumentOfGenericUserType"
+    )]
+    public void FullNameFromMethod(string methodName, string expectedFullName)
+    {
+        var method = typeof(MyClass).GetMethod(
+            methodName,
+            BindingFlags.Instance | BindingFlags.NonPublic
+        );
+        
+        var actualFullName = IdFunctions.CreateFullName(method);
+
+        Assert.That(actualFullName, Is.EqualTo(expectedFullName));
+    }
+
+    [Test]
+    public void TestFullNameFromMethodOfNestedGenericClass()
+    {
+        var method = typeof(MyClass<>).GetMethod(
+            nameof(MyClass<int>.GenericMethodOfGenericClass),
+            BindingFlags.Instance | BindingFlags.NonPublic
+        );
+
+        var actualFullName = IdFunctions.CreateFullName(method);
+
+        Assert.That(actualFullName, Is.EqualTo(
+            "Allure.Net.Commons.Tests:Allure.Net.Commons.Tests.FunctionTests." +
+                "IdTests+MyClass`1[T].GenericMethodOfGenericClass[V]" +
+                "(System.Collections.Generic.List`1[T],System.Collections." +
+                "Generic.List`1[V])"
+        ));
+    }
+}
---
+++ Allure.Net.Commons/Functions/IdFunctions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Allure.Net.Commons.Functions;
+
+/// <summary>
+/// Functions to create UUIDs, full names, etc.
+/// </summary>
+public static class IdFunctions
+{
+    /// <summary>
+    /// Generates a UUID for a test or container.
+    /// </summary>
+    public static string CreateUUID() => Guid.NewGuid().ToString();
+
+    /// <summary>
+    /// Creates a string that unuquely describes a given method.
+    /// </summary>
+    /// <remarks>
+    /// For a given test method the full name includes:
+    /// <list type="bullet">
+    /// <item>
+    /// fully-qualified name of the declaring type (including type parameters)
+    /// </item>
+    /// <item>name of the method</item>
+    /// <item>generic parameters of the method</item>
+    /// <item>
+    /// fully-qualified names of the parameter types, (including parameter
+    /// modifiers, if any)
+    /// </item>
+    /// </list>
+    /// A fully-qualified name of a type includes the assembly name, the
+    /// namespace and the class name (can be a nested class).
+    /// </remarks>
+    public static string CreateFullName(MethodBase method)
+    {
+        var className = SerializeType(method.DeclaringType);
+        var methodName = method.Name;
+        var typeParameters = method.GetGenericArguments();
+        var typeParametersDecl = SerializeTypeParameterTypeList(typeParameters);
+        var parameterTypes = SerializeParameterTypes(method.GetParameters());
+        return $"{className}.{methodName}{typeParametersDecl}({parameterTypes})";
+    }
+
+    static string SerializeParameterTypes(
+        IEnumerable<ParameterInfo> parameters
+    ) =>
+        SerializeTypeList(
+            parameters.Select(p => p.ParameterType)
+        );
+
+    static string SerializeTypeParameterTypeList(IEnumerable<Type> types) =>
+        types.Any() ? SerializeNonEmptyTypeParameterTypeList(types) : "";
+
+    static string SerializeNonEmptyTypeParameterTypeList(IEnumerable<Type> types) =>
+        "[" + SerializeTypeList(types) + "]";
+
+    static string SerializeTypeList(
+        IEnumerable<Type> types
+    ) =>
+        string.Join(
+            ",",
+            types.Select(SerializeType)
+        );
+
+    static string SerializeType(Type type)
+    {
+        if (type.IsGenericParameter)
+        {
+            return type.Name;
+        }
+        return GetUniqueTypeName(type) + SerializeTypeParameterTypeList(
+            type.GetGenericArguments()
+        );
+    }
+
+    static string GetUniqueTypeName(Type type) =>
+        IsSystemType(type)
+            ? ResolveFullName(type)
+            : GetTypeNameWithAssembly(type);
+
+    static string ResolveFullName(Type type) =>
+        type.FullName ?? ConstructFullName(type);
+
+    static string ConstructFullName(Type type) =>
+        type.DeclaringType is null
+            ? ConstructFullNameOfRootClass(type)
+            : ConstructFullNameOfNestedClass(type);
+
+    static string ConstructFullNameOfNestedClass(Type type) =>
+        ConstructFullName(type.DeclaringType) + "+" + type.Name;
+
+    static string ConstructFullNameOfRootClass(Type type) =>
+        string.IsNullOrEmpty(type.Namespace)
+            ? type.Name
+            : $"{type.Namespace}.{type.Name}";
+
+    static string GetTypeNameWithAssembly(Type type) =>
+        $"{type.Assembly.GetName().Name}:" + ResolveFullName(type);
+
+    static bool IsSystemType(Type type) =>
+        type.Assembly == systemTypesAssembly;
+
+    static readonly Assembly systemTypesAssembly = typeof(object).Assembly;
+}
