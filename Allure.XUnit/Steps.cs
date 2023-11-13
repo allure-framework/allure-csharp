94e483b64c10f26bcece23ebc8fd15ed08d63be0
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2023-11-14 01:40:29 +0700

    Fix AllureApi name referred from Steps obsolete msg

--- Allure.XUnit/Steps.cs
+++ Allure.XUnit/Steps.cs
@@ -4,7 +4,7 @@ using Allure.Net.Commons.Steps;
 
 namespace Allure.Xunit
 {
-    [Obsolete("Members of this class are now a part of the end user API represented by the Allure facade. " +
+    [Obsolete("Members of this class are now a part of the end user API represented by the AllureApi facade. " +
         "Please, use the Allure.Net.Commons.Allure class instead.")]
     [EditorBrowsable(EditorBrowsableState.Never)]
     public abstract class Steps : CoreStepsHelper
