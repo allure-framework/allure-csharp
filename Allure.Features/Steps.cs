9fdc26d43e8fd77d6a1258753d64428996ebaf09
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2024-03-27 22:25:32 +0700

    Format Allure.Features.Steps

--- Allure.Features/Steps.cs
+++ Allure.Features/Steps.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using Allure.Net.Commons;
 using Allure.SpecFlowPlugin;
 using NUnit.Framework;
@@ -9,115 +8,117 @@ using TechTalk.SpecFlow;
 
 namespace Allure.Features
 {
-  public enum TestOutcome
-  {
-    passed,
-    failed,
-    broken
-  }
-
-  [Binding]
-  public class Steps
-  {
-    private static readonly AllureLifecycle allure = AllureLifecycle.Instance;
-
-    private FeatureContext featureContext;
-    private readonly ScenarioContext scenarioContext;
-
-    public Steps(FeatureContext featureContext, ScenarioContext scenarioContext)
+    public enum TestOutcome
     {
-      this.featureContext = featureContext;
-      this.scenarioContext = scenarioContext;
+        passed,
+        failed,
+        broken
     }
 
-    [StepDefinition(@"Step is '(.*)'")]
-    public void StepResultIs(TestOutcome outcome)
+    [Binding]
+    public class Steps
     {
-      switch (outcome)
-      {
-        case TestOutcome.passed:
-          break;
-        case TestOutcome.failed:
-          Assert.Fail("Failed");
-          break;
-        case TestOutcome.broken:
-          throw new Exception("This test is failed",
-            new InvalidOperationException("Internal message",
-              new ArgumentException("One more message")));
-        default:
-          throw new ArgumentException("value is not supported");
-      }
-    }
+        private readonly ScenarioContext scenarioContext;
 
-    [StepDefinition("Step with attachment")]
-    public void StepWithAttach()
-    {
-      var path = Guid.NewGuid().ToString();
-      File.WriteAllText(path, "hi there");
-      AllureApi.AddAttachment(path);
-    }
+        public Steps(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
 
-    [StepDefinition("Step with table")]
-    public void StepWithTable(Table table)
-    {
-    }
+        [StepDefinition(@"Step is '(.*)'")]
+        public void StepResultIs(TestOutcome outcome)
+        {
+            switch (outcome)
+            {
+                case TestOutcome.passed:
+                    break;
+                case TestOutcome.failed:
+                    Assert.Fail("Failed");
+                    break;
+                case TestOutcome.broken:
+                    throw new Exception("This test is failed",
+                      new InvalidOperationException("Internal message",
+                        new ArgumentException("One more message")));
+                default:
+                    throw new ArgumentException("value is not supported");
+            }
+        }
 
-    [StepDefinition("Step with params: (.*)")]
-    public void StepWithArgs(int number, string text)
-    {
-    }
+        [StepDefinition("Step with attachment")]
+        public void StepWithAttach()
+        {
+            var path = Guid.NewGuid().ToString();
+            File.WriteAllText(path, "hi there");
+            AllureApi.AddAttachment(path);
+        }
 
-    [BeforeTestRun]
-    public static void BeforeTestRun()
-    {
-      AllureLifecycle.Instance.CleanupResultDirectory();
-    }
-    [BeforeFeature("beforefeaturepassed", Order = 1)]
-    public static void PassedBeforeFeature()
-    {
-    }
+        [StepDefinition("Step with table")]
+        public void StepWithTable(Table table)
+        {
+        }
 
-    [AfterFeature("afterfeaturepassed", Order = 1)]
-    public static void PassedAfterFeature()
-    {
-    }
+        [StepDefinition("Step with params: (.*)")]
+        public void StepWithArgs(int number, string text)
+        {
+        }
 
-    [BeforeFeature("beforefeaturefailed")]
-    public static void FailedBeforeFeature()
-    {
-      throw new Exception("Failed Before Feature");
-    }
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            AllureLifecycle.Instance.CleanupResultDirectory();
+        }
+        [BeforeFeature("beforefeaturepassed", Order = 1)]
+        public static void PassedBeforeFeature()
+        {
+        }
 
-    [AfterFeature("afterfeaturefailed")]
-    public static void FailedAfterFeature()
-    {
-      throw new Exception("Failed After Feature");
-    }
+        [AfterFeature("afterfeaturepassed", Order = 1)]
+        public static void PassedAfterFeature()
+        {
+        }
 
-    [BeforeScenario(tags: "beforescenario")]
-    [AfterScenario(tags: "afterscenario")]
-    [BeforeStep(tags: "beforestep")]
-    [AfterStep(tags: "afterstep")]
-    public void HandleIt()
-    {
-      Handle(scenarioContext.ScenarioInfo.Tags);
-    }
+        [BeforeFeature("beforefeaturefailed")]
+        public static void FailedBeforeFeature()
+        {
+            throw new Exception("Failed Before Feature");
+        }
 
-    private static void Handle(string[] tags)
-    {
-      if (tags != null && tags.Contains("attachment"))
-      {
-        var path = $"{Guid.NewGuid().ToString()}.txt";
-        File.WriteAllText(path, "hi there");
-        AllureApi.AddAttachment(path);
-        AllureApi.AddAttachment(path, "text file");
-      }
+        [AfterFeature("afterfeaturefailed")]
+        public static void FailedAfterFeature()
+        {
+            throw new Exception("Failed After Feature");
+        }
 
-      if (tags != null)
-        if (tags.Any(x => x.EndsWith(Status.failed.ToString()) || x.EndsWith(Status.broken.ToString())))
-          throw new Exception("Wasted");
-        else if (tags.Any(x => x == PluginHelper.IGNORE_EXCEPTION))
-          throw new IgnoreException();
+        [BeforeScenario(tags: "beforescenario")]
+        [AfterScenario(tags: "afterscenario")]
+        [BeforeStep(tags: "beforestep")]
+        [AfterStep(tags: "afterstep")]
+        public void HandleIt()
+        {
+            Handle(scenarioContext.ScenarioInfo.Tags);
+        }
+
+        private static void Handle(string[] tags)
+        {
+            if (tags != null && tags.Contains("attachment"))
+            {
+                var path = $"{Guid.NewGuid().ToString()}.txt";
+                File.WriteAllText(path, "hi there");
+                AllureApi.AddAttachment(path);
+                AllureApi.AddAttachment(path, "text file");
+            }
+
+            if (tags != null)
+            {
+                if (tags.Any(x => x.EndsWith(Status.failed.ToString()) || x.EndsWith(Status.broken.ToString())))
+                {
+                    throw new Exception("Wasted");
+                }
+                else if (tags.Any(x => x == PluginHelper.IGNORE_EXCEPTION))
+                {
+                    throw new IgnoreException();
+                }
+            }
+        }
     }
-  }
 }
