00a9268570f6a7456477ca133c5a25aa5d388dbc
Anton <tonik.sam@gmail.com>
2024-03-28 15:26:15 +0500

    #441 - Fix exit code 1 if Harmony patching failled (#455)
    * #441 - Fix exit code 1 if Harmony patching failled
    
    Use LogWarning instead LogError
    Change codestyle - filescoped namespace, explicit modifiers for better code readability
    
    * #441 - Fix exit code 1 if Harmony patching failled
    
    Review fixes

--- Allure.XUnit/AllureXunitPatcher.cs
+++ Allure.XUnit/AllureXunitPatcher.cs
@@ -1,106 +1,107 @@
-using System;
-
 using HarmonyLib;
+using System;
 using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
-namespace Allure.XUnit
+namespace Allure.XUnit;
+
+internal static class AllureXunitPatcher
 {
-    static class AllureXunitPatcher
+    private const string ALLURE_ID = "io.qameta.allure.xunit";
+    private static bool _isPatched;
+    private static IRunnerLogger _logger;
+
+    private static AllureMessageSink CurrentSink
     {
-        const string ALLURE_ID = "io.qameta.allure.xunit";
-        static bool isPatched = false;
-        static IRunnerLogger logger;
-
-        static AllureMessageSink CurrentSink
+        get
         {
-            get
+            var sink = AllureMessageSink.CurrentSink;
+
+            if (sink is null)
             {
-                var sink = AllureMessageSink.CurrentSink;
-                if (sink is null)
-                {
-                    logger.LogWarning("Unable to get current message sink.");
-                }
-                return sink;
+                _logger.LogWarning("Unable to get current message sink.");
             }
+
+            return sink;
         }
-
-        public static void PatchXunit(IRunnerLogger runnerLogger)
-        {
-            if (isPatched)
-            {
-                logger.LogMessage(
-                    "Patching is skipped: Xunit is already patched"
-                );
-                return;
-            }
-
-            logger = runnerLogger;
-            var patcher = new Harmony(ALLURE_ID);
-            PatchXunitTestRunnerCtors(patcher);
-            isPatched = true;
-        }
-
-        static void PatchXunitTestRunnerCtors(Harmony patcher)
-        {
-            var testRunnerType = typeof(XunitTestRunner);
-            var wasPatched = false;
-            foreach (var ctor in testRunnerType.GetConstructors())
-            {
-                try
-                {
-                    patcher.Patch(
-                        ctor,
-                        prefix: new HarmonyMethod(
-                            typeof(AllureXunitPatcher),
-                            nameof(OnTestRunnerCreating)
-                        ),
-                        postfix: new HarmonyMethod(
-                            typeof(AllureXunitPatcher),
-                            nameof(OnTestRunnerCreated)
-                        )
-                    );
-                    wasPatched = true;
-                    logger.LogImportantMessage(
-                        "{0}'s {1} has been patched",
-                        testRunnerType.Name,
-                        ctor.ToString()
-                    );
-                }
-                catch (Exception e)
-                {
-                    logger.LogError(
-                        "Unable to patch {0}'s {1}: {2}",
-                        testRunnerType.Name,
-                        ctor.ToString(),
-                        e.ToString()
-                    );
-                }
-            }
-
-            if (!wasPatched)
-            {
-                logger.LogWarning(
-                    "No constructors of {0} were pathched. Some theories may " +
-                        "miss their parameters in the report",
-                    testRunnerType.Name
-                );
-            }
-        }
-
-        static void OnTestRunnerCreating(ITest test, ref string skipReason)
-        {
-            if (!CurrentSink.SelectByTestPlan(test))
-            {
-                skipReason = "Deselected by the testplan.";
-            }
-        }
-
-        static void OnTestRunnerCreated(
-            ITest test,
-            object[] testMethodArguments
-        ) =>
-            CurrentSink.OnTestArgumentsCreated(test, testMethodArguments);
     }
+
+    public static void PatchXunit(IRunnerLogger runnerLogger)
+    {
+        if (_isPatched)
+        {
+            _logger.LogMessage(
+                "Patching is skipped: Xunit is already patched"
+            );
+            return;
+        }
+
+        _logger = runnerLogger;
+
+        var patcher = new Harmony(ALLURE_ID);
+        PatchXunitTestRunnerCtors(patcher);
+        _isPatched = true;
+    }
+
+    private static void PatchXunitTestRunnerCtors(Harmony patcher)
+    {
+        var testRunnerType = typeof(XunitTestRunner);
+        var wasPatched = false;
+
+        foreach (var ctor in testRunnerType.GetConstructors())
+        {
+            try
+            {
+                patcher.Patch(
+                    ctor,
+                    prefix: new HarmonyMethod(
+                        typeof(AllureXunitPatcher),
+                        nameof(OnTestRunnerCreating)
+                    ),
+                    postfix: new HarmonyMethod(
+                        typeof(AllureXunitPatcher),
+                        nameof(OnTestRunnerCreated)
+                    )
+                );
+
+                wasPatched = true;
+
+                _logger.LogImportantMessage(
+                    "{0}'s {1} has been patched",
+                    testRunnerType.Name,
+                    ctor.ToString()
+                );
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning(
+                    "Unable to patch {0}'s {1}: {2}",
+                    testRunnerType.Name,
+                    ctor.ToString(),
+                    e.ToString()
+                );
+            }
+        }
+
+        if (!wasPatched)
+        {
+            _logger.LogWarning(
+                "No constructors of {0} were patched. Some theories may " +
+                "miss their parameters in the report",
+                testRunnerType.Name
+            );
+        }
+    }
+
+    private static void OnTestRunnerCreating(ITest test, ref string skipReason)
+    {
+        if (!CurrentSink.SelectByTestPlan(test))
+        {
+            skipReason = "Deselected by the test plan.";
+        }
+    }
+
+    private static void OnTestRunnerCreated(ITest test, object[] testMethodArguments) => 
+        CurrentSink.OnTestArgumentsCreated(test, testMethodArguments);
 }
