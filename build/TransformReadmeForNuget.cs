271f363e6e2a76775a96488f2804bebf0f084139
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2024-06-03 18:41:28 +0700

    Remove unsupported links from NuGet README instead of replacing

--- build/TransformReadmeForNuget.cs
+++ build/TransformReadmeForNuget.cs
@@ -16,7 +16,6 @@ var imgLinkPattern = new Regex(
     @"\[<img(?<attrs>[^>]*)>\]\((?<url>[^ ]+) ""(?<title>[^""]+)""\)",
     RegexOptions.IgnoreCase | RegexOptions.Singleline
 );
-var attrsPattern = new Regex(@"(?<name>\w+)=[""'](?<value>[^""']+)[""']", RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
 using var inputReader = new StreamReader(
     inputFilePath,
@@ -32,65 +31,7 @@ using var outputWriter = new StreamWriter(
 for (string line; (line = inputReader.ReadLine()) is not null; )
 {
     var match = imgLinkPattern.Match(line);
-    if (match.Success)
-    {
-        var attrs = match.Groups["attrs"].Value;
-        var url = match.Groups["url"].Value;
-        var title = match.Groups["title"].Value;
-        string src = default;
-        string alt = default;
-        string height = default;
-        string width = default;
-        foreach (Match attrMatch in attrsPattern.Matches(attrs))
-        {
-            var attrName = attrMatch.Groups["name"].Value;
-            var attrValue = attrMatch.Groups["value"].Value;
-            switch (attrName)
-            {
-                case "src":
-                    src = attrValue;
-                    break;
-                case "alt":
-                    alt = attrValue;
-                    break;
-                case "height":
-                    height = Regex.Replace(attrValue, @"\D", "");
-                    break;
-                case "width":
-                    width = Regex.Replace(attrValue, @"\D", "");
-                    break;
-            }
-        }
-        var imgOriginalFileName = new Uri(src).Segments.Last();
-        var imgFileNameBuilder = new StringBuilder(
-            Path.GetFileNameWithoutExtension(imgOriginalFileName)
-        );
-        IEnumerable<string> dimensions = [width, height];
-        var dimSuffix = string.Join("x", dimensions.Where(v => v is not null));
-        if (dimSuffix != "")
-        {
-            imgFileNameBuilder.Append($"_{dimSuffix}");
-        }
-        var imgExtension = Path.GetExtension(imgOriginalFileName).ToLower();
-        imgFileNameBuilder.Append(
-            imgExtension.ToLower() == ".svg" ? ".png" : imgExtension
-        );
-        var imgFileName = imgFileNameBuilder.ToString();
-        var imgFsPath = Path.Combine(cwd, "..", "img", imgFileName);
-        if (!File.Exists(imgFsPath))
-        {
-            Log.LogError($"Nuget README converter: {this.InputReadmePath} - {imgFileName} doesn't exist in img");
-        }
-
-        outputWriter.WriteLine(
-            "[![{0}]({1} \"{2}\")]({3})",
-            alt,
-            $"https://raw.githubusercontent.com/allure-framework/allure-csharp/main/img/{imgFileName}",
-            title,
-            url
-        );
-    }
-    else
+    if (!imgLinkPattern.IsMatch(line))
     {
         outputWriter.WriteLine(line);
     }
