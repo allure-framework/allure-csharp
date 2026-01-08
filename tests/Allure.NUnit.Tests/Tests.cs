accc277a311ef0a73ae29049f7c5f9a9bf868778
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-01-09 03:48:44 +0700

    test(nunit): system test PoC

--- tests/Allure.NUnit.Tests/Tests.cs
+++ tests/Allure.NUnit.Tests/Tests.cs
@@ -1,65 +1,22 @@
-﻿using Allure.NUnit.Tests.Data;
+﻿using System.Text.Json.Nodes;
+using Allure.Testing;
 
 namespace Allure.NUnit.Tests;
 
-public class Tests
+internal class LabelTests
 {
     [Test]
-    public void Basic()
+    public async Task AttributeLabelPassedToResult()
     {
-        Console.WriteLine("This is a basic test");
-    }
+        var runResult = await SampleRunner.RunAsync(AllureSampleRegistry.AttributeLabel);
 
-    [Test]
-    [Arguments(1, 2, 3)]
-    [Arguments(2, 3, 5)]
-    public async Task DataDrivenArguments(int a, int b, int c)
-    {
-        Console.WriteLine("This one can accept arguments from an attribute");
-
-        var result = a + b;
-
-        await Assert.That(result).IsEqualTo(c);
-    }
-
-    [Test]
-    [MethodDataSource(nameof(DataSource))]
-    public async Task MethodDataSource(int a, int b, int c)
-    {
-        Console.WriteLine("This one can accept arguments from a method");
-
-        var result = a + b;
-
-        await Assert.That(result).IsEqualTo(c);
-    }
-
-    [Test]
-    [ClassDataSource<DataClass>]
-    [ClassDataSource<DataClass>(Shared = SharedType.PerClass)]
-    [ClassDataSource<DataClass>(Shared = SharedType.PerAssembly)]
-    [ClassDataSource<DataClass>(Shared = SharedType.PerTestSession)]
-    public void ClassDataSource(DataClass dataClass)
-    {
-        Console.WriteLine("This test can accept a class, which can also be pre-initialised before being injected in");
-
-        Console.WriteLine("These can also be shared among other tests, or new'd up each time, by using the `Shared` property on the attribute");
-    }
-
-    [Test]
-    [DataGenerator]
-    public async Task CustomDataGenerator(int a, int b, int c)
-    {
-        Console.WriteLine("You can even define your own custom data generators");
-
-        var result = a + b;
-
-        await Assert.That(result).IsEqualTo(c);
-    }
-
-    public static IEnumerable<(int a, int b, int c)> DataSource()
-    {
-        yield return (1, 1, 2);
-        yield return (2, 1, 3);
-        yield return (3, 1, 4);
+        await Assert.That(runResult.ExitCode).IsZero();
+        var nodes = runResult.AllureResults.TestResults[0]["labels"].AsArray().Cast<JsonObject>();
+        await Assert.That(nodes).Any(
+            l =>
+            {
+                return (string)l["name"] == "foo" && (string)l["value"] == "bar";
+            }
+        );
     }
 }
