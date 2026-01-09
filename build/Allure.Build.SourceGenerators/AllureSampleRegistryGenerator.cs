bbc97a28b8a6764212b3aeffcc7a8815493a2d3e
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-01-10 04:05:18 +0700

    test: fix sample dir handling by registry generator

---
+++ build/Allure.Build.SourceGenerators/AllureSampleRegistryGenerator.cs
@@ -0,0 +1,198 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Allure.Build.SourceGenerators;
+
+using SampleProjectSources = ImmutableArray<(string, ImmutableArray<string>)>;
+
+[Generator]
+public class AllureSampleRegistryGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        IncrementalValueProvider<SampleProjectSources> sampleSourceStream
+            = SetupGroupedSampleSourcesStream(context);
+
+        context.RegisterSourceOutput(sampleSourceStream, GenerateSampleRegistry);
+    }
+
+    static IncrementalValueProvider<SampleProjectSources> SetupGroupedSampleSourcesStream(
+        IncrementalGeneratorInitializationContext context
+    ) =>
+        context.AdditionalTextsProvider
+            .Combine(context.AnalyzerConfigOptionsProvider)
+            .Select(PairWithSuffix)
+            .Where(IsSampleSourceWellDefined)
+            .Collect()
+            .Select(GroupSampleSourcesBySuffix);
+
+    static (string, string) PairWithSuffix(
+        (AdditionalText Left, AnalyzerConfigOptionsProvider Right) pair,
+        CancellationToken _
+    ) =>
+        pair.Right
+            .GetOptions(pair.Left)
+            .TryGetValue("build_metadata.AdditionalFiles.Allure_ProjectSuffix", out var value)
+                && SyntaxFacts.IsValidIdentifier(value)
+                ? (value, pair.Left.Path)
+                : ("", "");
+
+    static bool IsSampleSourceWellDefined((string, string) pair) =>
+        pair.Item1 is not "" && pair.Item2 is not "";
+
+    static SampleProjectSources GroupSampleSourcesBySuffix(
+        ImmutableArray<(string, string)> sampleSources,
+        CancellationToken _
+    ) =>
+        [
+            .. sampleSources
+                .GroupBy(
+                    (pair) => pair.Item1,
+                    (pair) => pair.Item2,
+                    (key, values) => (key, values.ToImmutableArray())
+                )
+        ];
+
+    static void GenerateSampleRegistry(
+        SourceProductionContext productionContext,
+        SampleProjectSources sampleSources
+    )
+    {
+        var code = GetSampleRegistryCode(sampleSources);
+
+        productionContext.AddSource("AllureSampleRegistry.g.cs", code);
+    }
+
+    static string GetSampleRegistryCode(SampleProjectSources sampleSources)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine("namespace Allure.Testing;");
+        sb.AppendLine();
+        AppendRegistryEntryClass(sb);
+        sb.AppendLine();
+        AppendRegistryClass(sb, sampleSources);
+        return sb.ToString();
+    }
+
+    static void AppendRegistryEntryClass(StringBuilder sb)
+    {
+        sb.Append(
+            """
+            /// <summary>
+            /// Contains the data required to run a specific sample project and access its result files.
+            /// </summary>
+            /// <remarks>
+            /// Use the instances exposed by <see cref="global::Allure.Testing.AllureSampleRegistry"/>
+            /// instead of creating your own.
+            /// </remarks>
+            internal record class AllureSampleRegistryEntry(
+                string Name,
+                string ProjectPath,
+                string DefaultResultsPath
+            );
+
+            """
+        );
+    }
+
+    static void AppendRegistryClass(StringBuilder sb, SampleProjectSources sampleSources)
+    {
+        sb.Append(
+            $$"""
+            /// <summary>
+            /// Exposes a set of testing samples available to this project via a set of static properties.
+            /// </summary>
+            /// <remarks>
+            /// Pass a selected sample to <see cref="global::Allure.Testing.AllureSampleRunner"/> methods
+            /// to run it and access the test results.
+            /// </remarks>
+            internal static class AllureSampleRegistry
+            {
+
+            """
+        );
+        AppendRegistryEntryProperties(sb, sampleSources);
+        sb.AppendLine("}");
+    }
+
+    static void AppendRegistryEntryProperties(StringBuilder sb, SampleProjectSources sampleSources)
+    {
+        if (sampleSources.Length > 0)
+        {
+            var (suffix, files) = sampleSources[0];
+            AppendRegistryEntryProperty(sb, suffix, files);
+        }
+
+        foreach (var (suffix, files) in sampleSources.Skip(1))
+        {
+            sb.AppendLine();
+            AppendRegistryEntryProperty(sb, suffix, files);
+        }
+    }
+
+    static void AppendRegistryEntryProperty(StringBuilder sb, string suffix, ImmutableArray<string> files)
+    {
+        var pathPrefix = files.Length == 1 ? files[0] : GetGreatestCommonPrefix(files);
+
+        sb.Append(
+            $$"""
+                /// <summary>
+                /// <a href="file://{{ pathPrefix }}">
+                /// {{ pathPrefix }}
+                /// </a>
+                /// </summary>
+                public static global::Allure.Testing.AllureSampleRegistryEntry {{ suffix }} { get; }
+                    = new(
+                        "{{ suffix }}",
+                        global::System.IO.Path.Combine(
+                            global::Allure.Testing.AllureBuildProperties.Allure_SampleSolutionDir,
+                            string.Format(
+                                "{0}.{{ suffix }}",
+                                global::Allure.Testing.AllureBuildProperties.Allure_SampleSolutionName
+                            ),
+                            string.Format(
+                                "{0}.{{ suffix }}.csproj",
+                                global::Allure.Testing.AllureBuildProperties.Allure_SampleSolutionName
+                            )
+                        ),
+                        string.Format(
+                            global::Allure.Testing.AllureBuildProperties.Allure_SampleResultsDirectoryFormat,
+                            "{{ suffix }}"
+                        )
+                    );
+
+            """
+        );
+    }
+
+    static string GetGreatestCommonPrefix(IEnumerable<string> paths)
+    {
+        var first = paths.First();
+        var rest = paths.Skip(1).ToList();
+
+        string prefix = first;
+
+        while ((prefix = Path.GetDirectoryName(prefix)) is not null)
+        {
+            if (IsCommonPrefix(rest, prefix))
+            {
+                return prefix;
+            }
+        }
+
+        return "";
+    }
+
+    static bool IsCommonPrefix(List<string> files, string prefix) =>
+        files.All((path) =>
+            path.StartsWith(prefix)
+                && path.Length > prefix.Length
+                && path[prefix.Length] == Path.DirectorySeparatorChar);
+}
