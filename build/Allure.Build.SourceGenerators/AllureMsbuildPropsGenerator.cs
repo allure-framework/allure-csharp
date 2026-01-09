1312f68208c1abe885d0d46d64b8a4c0caaf940b
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-01-10 02:09:42 +0700

    refactor: simplify props generator

--- build/Allure.Build.SourceGenerators/AllureMsbuildPropsGenerator.cs
+++ build/Allure.Build.SourceGenerators/AllureMsbuildPropsGenerator.cs
@@ -1,58 +1,100 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Allure.Build.SourceGenerators;
+
+using AllureBuildPropertiesData = ImmutableArray<(string, string)>;
 
 [Generator]
 public class AllureMsbuildPropsGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        var allureMsbuildProps = context
+        var allureMsbuildProps = SetupMsbuildPropsArrayStream(context);
+
+        context.RegisterSourceOutput(allureMsbuildProps, GenerateAllureBuildProperties);
+    }
+
+    static IncrementalValueProvider<AllureBuildPropertiesData> SetupMsbuildPropsArrayStream(
+        IncrementalGeneratorInitializationContext context
+    ) =>
+        context
             .AnalyzerConfigOptionsProvider
-            .SelectMany(static (c, _) =>
-                c.GlobalOptions.TryGetValue("build_property.Allure_PropertyNames", out var exports)
-                    ? exports
-                        .Split(':')
-                        .Select(k =>
-                            c.GlobalOptions.TryGetValue($"build_property.{k}", out var value)
-                                ? (k, value)
-                                : (k, null))
-                        .Where(static v => v.value is not null) as IEnumerable<(string, string)>
-                    : [])
+            .SelectMany(GetExportedAllureProperties)
             .Collect();
 
-        context.RegisterSourceOutput(
-            allureMsbuildProps,
-            static (spc, data) =>
+    static IEnumerable<string> GetAllurePropertyNames(AnalyzerConfigOptions options) =>
+        options.TryGetValue("build_property.Allure_PropertyNames", out var value)
+            ? value.Split(':')
+            : [];
+
+    static IEnumerable<(string, string)> GetExportedAllureProperties(
+        AnalyzerConfigOptionsProvider provider,
+        CancellationToken _
+    )
+    {
+        var options = provider.GlobalOptions;
+        foreach (var propertyName in GetAllurePropertyNames(options))
+        {
+            var edConfPropertyName = $"build_property.{propertyName}";
+            if (options.TryGetValue(edConfPropertyName, out var propertyValue))
             {
-                var sb = new StringBuilder();
-                sb.AppendLine("namespace Allure.Testing;");
-                sb.AppendLine();
-                sb.AppendLine("internal static class AllureBuildProperties");
-                sb.AppendLine("{");
-                bool emitNewLine = false;
-                foreach (var (key, value) in data)
-                {
-                    if (emitNewLine)
-                    {
-                        sb.AppendLine();
-                    }
-                    sb.AppendFormat("    public static string {0} {{ get; }}", key);
-                    sb.AppendLine();
-                    sb.AppendFormat("        = global::System.Environment.GetEnvironmentVariable($\"{0}\")", key);
-                    sb.AppendLine();
-                    sb.AppendFormat("            ?? {0};", SymbolDisplay.FormatLiteral(value, true));
-                    sb.AppendLine();
-                }
-                sb.AppendLine("}");
-
-
-                spc.AddSource("AllureBuildProperties.g.cs", sb.ToString());
+                yield return (propertyName, propertyValue);
             }
-        );
+        }
+    }
+
+    static void GenerateAllureBuildProperties(
+        SourceProductionContext productionContext,
+        AllureBuildPropertiesData data
+    )
+    {
+        var sourceCode = GetAllureBuildPropertiesSourceCode(data);
+        productionContext.AddSource(Constants.MSBUILD_PROPS_FILENAME, sourceCode);
+    }
+
+    static string GetAllureBuildPropertiesSourceCode(AllureBuildPropertiesData data)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"namespace {Constants.NAMESPACE_NAME};");
+        sb.AppendLine();
+        sb.AppendLine($"internal static class {Constants.MSBUILD_PROPS_CLASSNAME}");
+        sb.AppendLine("{");
+        AppendClassProperties(sb, data);
+        sb.AppendLine("}");
+
+        return sb.ToString();
+    }
+
+    static void AppendClassProperties(StringBuilder sb, AllureBuildPropertiesData data)
+    {
+        if (data.Length > 0)
+        {
+            AppendOneClassProperty(sb, data[0]);
+        }
+
+        foreach (var rest in data.Skip(1))
+        {
+            sb.AppendLine();
+            AppendOneClassProperty(sb, rest);
+        }
+    }
+
+    static void AppendOneClassProperty(StringBuilder sb, (string, string) data)
+    {
+        var (key, value) = data;
+
+        sb.AppendFormat("    public static string {0} {{ get; }}", key);
+        sb.AppendLine();
+        sb.AppendFormat("        = global::System.Environment.GetEnvironmentVariable($\"{0}\")", key);
+        sb.AppendLine();
+        sb.AppendFormat("            ?? {0};", SymbolDisplay.FormatLiteral(value, true));
+        sb.AppendLine();
     }
 }
---
+++ build/Allure.Build.SourceGenerators/Constants.cs
@@ -0,0 +1,8 @@
+namespace Allure.Build.SourceGenerators;
+
+internal static class Constants
+{
+    public const string MSBUILD_PROPS_FILENAME = "AllureBuildProperties.g.cs";
+    public const string MSBUILD_PROPS_CLASSNAME = "AllureBuildProperties";
+    public const string NAMESPACE_NAME = "Allure.Testing";
+}
