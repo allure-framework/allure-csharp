e9598e6eb052f22c427d26958782189878e477b5
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-01-12 16:20:23 +0700

    test: order sample run input props alphabetically

--- tests/Allure.Testing/AllureSampleRunInput.cs
+++ tests/Allure.Testing/AllureSampleRunInput.cs
@@ -8,11 +8,6 @@ namespace Allure.Testing;
 public record class AllureSampleRunInput
 {
     /// <summary>
-    /// A list of extra CLI arguments passed to <c>dotnet test</c> or <c>dotnet run</c>.
-    /// </summary>
-    public List<string> ProcessArguments { get; init; } = [];
-
-    /// <summary>
     /// An object that defines the content of <c>allureConfig.json</c>. Some supported values
     /// of this parameter include <c>System.Text.Json.Nodes.JsonObject</c> instances, dictionaries,
     /// and anonymous objects.
@@ -24,6 +19,11 @@ public record class AllureSampleRunInput
     /// <c>dotnet test</c> or <c>dotnet run</c>.
     /// </summary>
     public Dictionary<string, string> EnvironmentVariables { get; init; } = [];
+
+    /// <summary>
+    /// A list of extra CLI arguments passed to <c>dotnet test</c> or <c>dotnet run</c>.
+    /// </summary>
+    public List<string> ProcessArguments { get; init; } = [];
 
     public static AllureSampleRunInput Default { get; } = new();
 }
