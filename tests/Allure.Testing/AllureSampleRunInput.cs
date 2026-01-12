c9882f4743cd6eda9bba26dbc44ce8b1390e29a3
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-01-12 16:19:37 +0700

    test: make sample run input easier to create

--- tests/Allure.Testing/AllureSampleRunInput.cs
+++ tests/Allure.Testing/AllureSampleRunInput.cs
@@ -5,23 +5,25 @@ namespace Allure.Testing;
 /// <summary>
 /// Contains data that affects the execution of the sample project.
 /// </summary>
-/// <param name="ProcessArguments">
-/// A list of extra CLI arguments passed to <c>dotnet test</c> or <c>dotnet run</c>.
-/// </param>
-/// <param name="AllureConfiguration">
-/// An object that defines the content of <c>allureConfig.json</c>. Some supported values
-/// of this parameter include <c>System.Text.Json.Nodes.JsonObject</c> instances, dictionaries, and
-/// anonymous objects.
-/// </param>
-/// <param name="EnvironmentVariables">
-/// A dictionary that defines extra environment variables passed to
-/// <c>dotnet test</c> or <c>dotnet run</c>.
-/// </param>
-public record class AllureSampleRunInput(
-    List<string> ProcessArguments,
-    object? AllureConfiguration,
-    Dictionary<string, string> EnvironmentVariables
-)
+public record class AllureSampleRunInput
 {
-    public static AllureSampleRunInput Default { get; } = new([], null, []);
+    /// <summary>
+    /// A list of extra CLI arguments passed to <c>dotnet test</c> or <c>dotnet run</c>.
+    /// </summary>
+    public List<string> ProcessArguments { get; init; } = [];
+
+    /// <summary>
+    /// An object that defines the content of <c>allureConfig.json</c>. Some supported values
+    /// of this parameter include <c>System.Text.Json.Nodes.JsonObject</c> instances, dictionaries,
+    /// and anonymous objects.
+    /// </summary>
+    public object? AllureConfiguration { get; init; } = null;
+
+    /// <summary>
+    /// A dictionary that defines extra environment variables passed to
+    /// <c>dotnet test</c> or <c>dotnet run</c>.
+    /// </summary>
+    public Dictionary<string, string> EnvironmentVariables { get; init; } = [];
+
+    public static AllureSampleRunInput Default { get; } = new();
 }
