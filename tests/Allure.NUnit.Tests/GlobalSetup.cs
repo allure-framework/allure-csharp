39d2c8497a001780d2901831c497dc42ac18eb1b
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-01-19 21:54:37 +0700

    chore: remove redundant code

--- tests/Allure.NUnit.Tests/GlobalSetup.cs
+++ tests/Allure.NUnit.Tests/GlobalSetup.cs
@@ -9,18 +9,3 @@
 #endif
 
 namespace Allure.NUnit.Tests;
-
-public class GlobalHooks
-{
-    [Before(TestSession)]
-    public static void SetUp()
-    {
-
-    }
-
-    [After(TestSession)]
-    public static void CleanUp()
-    {
-
-    }
-}
