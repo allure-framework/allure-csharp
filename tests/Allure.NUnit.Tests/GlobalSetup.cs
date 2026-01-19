a5b8be613609ce98a01bba61bcde71ebf7234f64
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-01-19 19:27:35 +0700

    test(nunit): enable parallel in assertion-only mode

--- tests/Allure.NUnit.Tests/GlobalSetup.cs
+++ tests/Allure.NUnit.Tests/GlobalSetup.cs
@@ -2,7 +2,11 @@
 
 [assembly: System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 
+#if !ALLURE_TEST_ASSERT_ONLY
+
 [assembly: NotInParallel(["Allure.NUnit", "Allure.Net.Commons"])]
+
+#endif
 
 namespace Allure.NUnit.Tests;
 
