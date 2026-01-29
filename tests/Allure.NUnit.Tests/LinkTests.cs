4b1e2e1314ff37516499e687ea37e77b43ce9826
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-01-29 20:13:46 +0700

    test(nunit): add issue tests

--- tests/Allure.NUnit.Tests/LinkTests.cs
+++ tests/Allure.NUnit.Tests/LinkTests.cs
@@ -78,4 +78,70 @@ class LinkTests
                 && (string)l["name"] == "name-4"
                 && (string)l["type"] == "type-4");
     }
+
+    [Test]
+    public async Task CheckIssuesRuntimeApiWorks()
+    {
+        var results = await AllureSampleRunner.RunAsync(AllureSampleRegistry.AddIssuesAtRuntime);
+
+        await Assert.That(results.TestResults.Cast<JsonObject>()).Count().IsEqualTo(1);
+        var links = results.TestResults[0]["links"].AsArray().Cast<JsonObject>().ToList();
+        await Assert.That(links.Count).IsEqualTo(3);
+        await Assert.That(links[0]).Satisfies(static (l) =>
+            (string)l["url"] == "url-1"
+                && l["name"] is null
+                && (string)l["type"] == "issue");
+        await Assert.That(links[1]).Satisfies(static (l) =>
+            (string)l["url"] == "url-2"
+                && (string)l["name"] == "name-2"
+                && (string)l["type"] == "issue");
+        await Assert.That(links[2]).Satisfies(static (l) =>
+            (string)l["url"] == "url-3"
+                && (string)l["name"] == "name-3"
+                && (string)l["type"] == "issue");
+    }
+
+    [Test]
+    public async Task CheckLegacyIssueAttributesWork()
+    {
+        var results = await AllureSampleRunner.RunAsync(AllureSampleRegistry.LegacyIssueAttributes);
+
+        await Assert.That(results.TestResults.Cast<JsonObject>()).Count().IsEqualTo(1);
+        var links = results.TestResults[0]["links"].AsArray().Cast<JsonObject>().ToList();
+        await Assert.That(links.Count).IsEqualTo(3);
+        await Assert.That(links[0]).Satisfies(static (l) =>
+            (string)l["url"] == "url-3"
+                && (string)l["name"] == "url-3"
+                && (string)l["type"] == "issue");
+        await Assert.That(links[1]).Satisfies(static (l) =>
+            (string)l["url"] == "url-2"
+                && (string)l["name"] == "name-2"
+                && (string)l["type"] == "issue");
+        await Assert.That(links[2]).Satisfies(static (l) =>
+            (string)l["url"] == "url-1"
+                && (string)l["name"] == "url-1"
+                && (string)l["type"] == "issue");
+    }
+
+    [Test]
+    public async Task CheckIssueAttributesWork()
+    {
+        var results = await AllureSampleRunner.RunAsync(AllureSampleRegistry.IssueAttributes);
+
+        await Assert.That(results.TestResults.Cast<JsonObject>()).Count().IsEqualTo(1);
+        var links = results.TestResults[0]["links"].AsArray().Cast<JsonObject>().ToList();
+        await Assert.That(links.Count).IsEqualTo(3);
+        await Assert.That(links[0]).Satisfies(static (l) =>
+            (string)l["url"] == "url-1"
+                && l["name"] is null
+                && (string)l["type"] == "issue");
+        await Assert.That(links[1]).Satisfies(static (l) =>
+            (string)l["url"] == "url-2"
+                && (string)l["name"] == "name-2"
+                && (string)l["type"] == "issue");
+        await Assert.That(links[2]).Satisfies(static (l) =>
+            (string)l["url"] == "url-3"
+                && l["name"] is null
+                && (string)l["type"] == "issue");
+    }
 }
