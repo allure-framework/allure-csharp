b5043a27e4768e618055f3632e9263054b91df61
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-01-30 21:24:57 +0700

    test(nunit): add meta attribute tests

---
+++ tests/Allure.NUnit.Tests/Samples/MetaAttributes.cs
@@ -0,0 +1,43 @@
+using System;
+using Allure.Net.Commons;
+using Allure.Net.Commons.Attributes;
+using Allure.NUnit;
+using NUnit.Framework;
+
+namespace Allure.NUnit.Tests.Samples.MetaAttributes
+{
+    [AllureEpic("Foo")]
+    [AllureOwner("John Doe")]
+    [AttributeUsage(AttributeTargets.Interface)]
+    public class EpicOwnerAttribute : AllureMetaAttribute { }
+
+    [AllureFeature("Bar")]
+    [AllureTag("foo", "bar")]
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FeatureTagsAttribute : AllureMetaAttribute { }
+
+    [AllureStory("Baz")]
+    [AllureLink("https://foo.bar/")]
+    [AttributeUsage(AttributeTargets.Class)]
+    public class StoryLinkAttribute : AllureMetaAttribute { }
+
+    [AllureSeverity(SeverityLevel.critical)]
+    [AllureSuite("Qux")]
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SeveritySuiteAttribute : AllureMetaAttribute { }
+
+    [EpicOwner]
+    public interface IMetadata { }
+
+    [FeatureTags]
+    public class BaseClass { }
+
+    [AllureNUnit]
+    [StoryLink]
+    public class TestsClass : BaseClass, IMetadata
+    {
+        [Test]
+        [SeveritySuite]
+        public void TestMethod() { }
+    }
+}
