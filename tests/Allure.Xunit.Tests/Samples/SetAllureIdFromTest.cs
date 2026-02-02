567bfb53c8628be52beb5d19bab05a5c05ece472
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-02-02 15:12:33 +0700

    test(xunit): add allure id tests

---
+++ tests/Allure.Xunit.Tests/Samples/SetAllureIdFromTest.cs
@@ -0,0 +1,14 @@
+using Allure.Net.Commons;
+using Xunit;
+
+namespace Allure.Xunit.Tests.Samples.SetAllureIdFromTest
+{
+    public class TestsClass
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            AllureApi.SetAllureId(1001);
+        }
+    }
+}
