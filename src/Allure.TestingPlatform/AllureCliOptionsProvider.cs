e71de47c995bdf18f9440064bace7ba8c70d01b5
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-06-18 23:07:14 +0700

    fix(mtp): enable allure if no --allure provided

--- src/Allure.TestingPlatform/AllureCliOptionsProvider.cs
+++ src/Allure.TestingPlatform/AllureCliOptionsProvider.cs
@@ -44,6 +44,6 @@ public class AllureCliOptionsProvider() :
     }
 
     public static bool IsAllureEnabled(ICommandLineOptions options) =>
-        options.TryGetOptionArgumentList("allure", out var values)
-            && values[0] == "on";
+        !options.TryGetOptionArgumentList("allure", out var values)
+            || values[0] == "on";
 }
