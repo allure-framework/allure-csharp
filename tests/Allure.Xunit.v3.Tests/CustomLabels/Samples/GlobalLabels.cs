ed532cf0e7320456e66ca1563fa6e7462bc5b3ce
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-07-06 22:14:47 +0700

    test(xunit.v3): add global and env label tests

---
+++ tests/Allure.Xunit.v3.Tests/CustomLabels/Samples/GlobalLabels.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using Allure.Net.Commons.Attributes;
+
+namespace Allure.Xunit.v3.Tests.Samples.CustomLabels.GlobalLabels
+{
+    public class TestClass
+    {
+        [Fact]
+        public void TestMethod1() { }
+
+        [Fact]
+        public void TestMethod2() { }
+    }
+}
