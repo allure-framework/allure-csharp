f11db3083dad7f1211a440c4ab9f5f83c5c0bdae
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-07-06 17:53:12 +0700

    test(xunit.v3): introduce CancellationToken

--- tests/Allure.Xunit.v3.Tests/AllureIds/AllureIdTests.cs
+++ tests/Allure.Xunit.v3.Tests/AllureIds/AllureIdTests.cs
@@ -5,9 +5,9 @@ namespace Allure.Xunit.v3.Tests.AllureIds;
 class AllureIdTests
 {
     [Test]
-    public async Task CheckAllureIdAttributeWorks()
+    public async Task CheckAllureIdAttributeWorks(CancellationToken token)
     {
-        var results = await AllureSampleRunner.RunAsync(AllureSampleRegistry.AllureIdAttributeOnMethod);
+        var results = await AllureSampleRunner.RunAsync(AllureSampleRegistry.AllureIdAttributeOnMethod, token);
 
         await Assert.That(results).HasSingleTestResult()
             .That.HasSingleLabel("ALLURE_ID")
