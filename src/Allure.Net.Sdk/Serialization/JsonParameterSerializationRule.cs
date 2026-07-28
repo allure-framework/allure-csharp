30c073f324461672b06b9837edd3284b23faab48
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-07-29 03:44:26 +0700

    feat(sdk): serialize more types with ToString

--- src/Allure.Net.Sdk/Serialization/JsonParameterSerializationRule.cs
+++ src/Allure.Net.Sdk/Serialization/JsonParameterSerializationRule.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 
 namespace Allure.Sdk.Serialization;
 
@@ -27,13 +30,89 @@ public class JsonParameterSerializationRule(JsonSerializerOptions serializerOpti
         return false;
     }
 
-    public static JsonSerializerOptions CreateDefaultJsonOptions() => new()
+    public static JsonSerializerOptions CreateDefaultJsonOptions()
     {
-        Converters =
+        JsonSerializerOptions options = new ()
         {
-            new JsonStringEnumConverter(),
-        },
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-        ReferenceHandler = ReferenceHandler.IgnoreCycles,
-    };
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            Converters =
+            {
+                new JsonStringEnumConverter(),
+            },
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            ReferenceHandler = ReferenceHandler.IgnoreCycles,
+            TypeInfoResolver = new DefaultJsonTypeInfoResolver
+            {
+                Modifiers = { IgnoreDelegatePropertiesModifier },
+            },
+        };
+
+        options.Converters.Add(new ObjectToStringConverterFactory(options));
+        return options;
+    }
+
+    static void IgnoreDelegatePropertiesModifier(JsonTypeInfo typeInfo)
+    {
+        if (typeInfo.Kind != JsonTypeInfoKind.Object)
+        {
+            return;
+        }
+
+        for (int i = typeInfo.Properties.Count - 1; i >= 0; i--)
+        {
+            var property = typeInfo.Properties[i];
+
+            if (typeof(Delegate).IsAssignableFrom(property.PropertyType))
+            {
+                typeInfo.Properties.RemoveAt(i);
+            }
+        }
+    }
+
+    class ObjectToStringConverterFactory(JsonSerializerOptions contractOptions) : JsonConverterFactory
+    {
+        readonly JsonSerializerOptions contractOptions = new(contractOptions);
+
+        public override bool CanConvert(Type typeToConvert)
+        {
+            var typeInfo = this.contractOptions.GetTypeInfo(typeToConvert);
+            if (typeInfo.Kind != JsonTypeInfoKind.Object)
+            {
+                return false;
+            }
+
+            var toString = typeToConvert.GetMethod(
+                nameof(ToString),
+                BindingFlags.Instance | BindingFlags.Public,
+                binder: null,
+                types: Type.EmptyTypes,
+                modifiers: null
+            );
+
+            return toString?.DeclaringType != typeof(object);
+        }
+
+        public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            Type converterType =
+                typeof(ObjectToStringConverter<>).MakeGenericType(typeToConvert);
+            return (JsonConverter)Activator.CreateInstance(converterType)!;
+        }
+    }
+
+    private sealed class ObjectToStringConverter<T> : JsonConverter<T>
+    {
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value?.ToString());
+        }
+
+        public override T Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            throw new NotSupportedException($"This converter cannot deserialize.");
+        }
+    }
 }
